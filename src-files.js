e37c8e0f9319e3df3ff801f5a38f4717a8a20b28
johannst <johannst@users.noreply.github.com>
2023-10-22 12:29:22 +0000

    deploy: 474c2545cbb1af85a326a47e202fe1e6c450b496

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "juicebox_asm":["",[["insn",[],["add.rs","call.rs","dec.rs","jmp.rs","jnz.rs","jz.rs","mov.rs","nop.rs","ret.rs","test.rs"]]],["imm.rs","insn.rs","label.rs","lib.rs","prelude.rs","reg.rs","rt.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
